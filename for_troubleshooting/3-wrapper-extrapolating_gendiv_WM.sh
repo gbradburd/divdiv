3cd5bccd47cad92aa8fba7349316323109339d36
Rachel Toczydlowski <rhtoczydlowski@gmail.com>
2022-04-06 23:23:00 -0400

    trying to get WM model running on hpcc

---
+++ for_troubleshooting/3-wrapper-extrapolating_gendiv_WM.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#define variables:
+storagenode=/mnt/research/bradburd_lab/divdiv_working_popgen #path to main node where input files live
+
+jobname=run-gendivextrapWM #label for SLURM book-keeping
+executable=3-run-extrapolating_gendiv_WM.sbatch #script to run
+
+logfilesdir=logfiles_gendivextrap_WM #name of directory to create and then write log files to
+
+cpus=2 #number of CPUs to request/use per dataset
+ram_per_cpu=8G #amount of RAM to request/use per CPU
+time=168:00:00
+
+list_of_datasets=list-mpg.txt #name of dataset that we want to process
+
+copy_files_to_execute_node=yes #yes to copy input folder to tmp dir on execute node and load files into R from there, no to load files into R directly from where they live on cluster aka $indir below in this file
+minpropindivsscoredin=0.5 #percent of indivs that a locus must be present in to save
+
+#---------------------------------------------------------
+
+#check if logfiles directory has been created in submit dir yet; if not, make one
+if [ ! -d ./$logfilesdir ]; then mkdir ./$logfilesdir; fi
+
+
+#for each line of the list_of_datasets file: 
+#	execute job that removes low quality reads from each seq. file inside the directories specified in the list_of_datasets file  
+while read dataset
+do 
+	
+	#define label to give dataset
+	run_name=$(echo $dataset | cut -d " " -f1)
+
+	#define path to where downloaded raw/unprocessed sequence data live for this dataset
+	#and where clean/processed reads should be written to
+	indir=$storagenode/$run_name
+	outdir=$storagenode/$run_name/gendiv_data
+	figdir=$storagenode/gendiv-figures
+
+	#submit job to cluster
+	sbatch --job-name=$jobname \
+			--export=CPUS=$cpus,RUN_NAME=$run_name,STORAGENODE=$storagenode,INDIR=$indir,OUTDIR=$outdir,LOGFILESDIR=$logfilesdir,FIGDIR=$figdir,COPY_FILES_TO_EXECUTE_NODE=$copy_files_to_execute_node,MINPROPINDIVSSCOREDIN=$minpropindivsscoredin \
+			--cpus-per-task=$cpus \
+			--mem-per-cpu=$ram_per_cpu \
+			--output=./$logfilesdir/${jobname}_${run_name}_%A.out \
+			--error=./$logfilesdir/${jobname}_${run_name}_%A.err \
+			--time=$time \
+			$executable
+			
+	echo submitted dataset $run_name from $list_of_datasets
+		
+done < ./master_keys/$list_of_datasets
