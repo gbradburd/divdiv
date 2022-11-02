7e70a53c04a39aa2a50b4973bc07ea68175f20c2
Rachel Toczydlowski <rhtoczydlowski@gmail.com>
2022-11-02 10:58:55 -0400

    fixing sample matching/dropping btwn geo and geno matricies

--- scripts/exe_WM.R
+++ scripts/exe_WM.R
@@ -66,73 +66,92 @@ print(popgenfiles_list)
 
 for (loop.iter in 1:length(popgenfiles_list)) {
   
-  #get sample names
-  sampkey <- read.delim(paste0(indir,"/samplenamekey.txt"))
-  
-  #get pw distance matrix
-  load(paste0(indir, "/max_and_pw_dists", run_name, ".Robj", sep = ""))
-  #geoDist <- max_and_pw_dists$pw.gcd.genetic
-  geoDist <- max_and_pw_dists$pw.seadist.genetic
-  
+  #get bookkeeping variables
   popgenfile = popgenfiles_list[loop.iter]   
   stacksparams = popgenfile %>% strsplit(., split = "/") %>% as.data.frame() %>% .[nrow(.),] %>% 
     gsub("_popgenstats|Robj","",.) %>% gsub(run_name,"",.) %>% gsub(minPropIndivsScoredin,"",.) %>% 
     gsub("\\.","",.) %>% gsub("popgenstats_stacks_","",.)
-  
   print(paste0("working on run_name ", run_name, " and Stacks params ", stacksparams))
   #also print this to .err so we can tell when errors are thrown if they are
   cat(paste0("working on run_name ", run_name, " and Stacks params ", stacksparams, " now\n"), file = stderr())
   
-  load(popgenfile)
-  #pwp <- popgenstats$pwp$pwp
-  pwp <- popgenstats$pwp
-  hom <- 1-pwp
-cat("here1\n", file = stderr())
-  # add inbreeding
-  diag(hom) <- 1
-  flag <- 0
-  scl_min <- min(hom)
-  scl_max <- max(hom-scl_min)
-  tmphom <- (hom-scl_min)/scl_max
-  posdefcheck <- any(eigen(tmphom)$values < 0)
-  while(posdefcheck & flag < 100){
-  	diag(hom) <- diag(hom) + 1e-3
-    tmphom <- (hom-scl_min)/scl_max
-	posdefcheck <- any(eigen(tmphom)$values < 0)
-	flag <- flag + 1
-  }
-  if(posdefcheck){
-	  cat("error: I couldn't get the dataset to be positive definite\n")
-  }
-  cat("here2\n", file = stderr())
-  #se <- popgenstats$pwp$se
+  #get sample names
+  sampkey <- read.delim(paste0(indir,"/samplenamekey.txt"))
   
   #get number of polymorphic loci
   load(paste0(indir, "/bpstats.", minPropIndivsScoredin, ".", run_name, "_stacks_", stacksparams, "_BPstats.Robj"))
   Npolyloci = BPstats$nLoci
- cat("here3\n", file = stderr())  
-  # rename geoDist, keep just samps in pwp, and order to match pwp
-  #keep just distances for samples in pwp matrix
+  cat("here1\n", file = stderr())
+
+  #get pw geographic distance matrix
+  load(paste0(indir, "/max_and_pw_dists", run_name, ".Robj", sep = ""))
+  #geoDist <- max_and_pw_dists$pw.gcd.genetic
+  geoDist <- max_and_pw_dists$pw.seadist.genetic
+  # rename geoDist from SRR IDs to sampleX IDs
   geoDistnames <- colnames(geoDist) %>% as.data.frame() %>% dplyr::rename("run_acc_sra" = ".") %>% 
     dplyr::mutate(order = 1:dplyr::n()) %>% 
     base::merge(., sampkey %>% dplyr::select(run_acc_sra,sampid_assigned_for_bioinf), by = "run_acc_sra", all.x = T) %>% 
     arrange(order)
   geoDistnames <- geoDistnames$sampid_assigned_for_bioinf
- cat("here4\n", file = stderr()) 
- colnames(geoDist) <- geoDistnames
+  colnames(geoDist) <- geoDistnames
   rownames(geoDist) <- geoDistnames
-  geoDist <- geoDist[rownames(geoDist) %in% rownames(pwp), colnames(geoDist) %in% rownames(pwp)]
-  #make sure all matrices are ordered the same
-  order <- rownames(pwp)
-  #geoDist <- graph4lg::reorder_mat(mat = geoDist, order = order)
-  geoDist.ordered <- geoDist[order, order]
+  cat("here2\n", file = stderr()) 
+
+  #get pw pi aka genetic matrix
+  load(popgenfile)
+  #pwp <- popgenstats$pwp$pwp
+  pwp <- popgenstats$pwp
+  cat("here3\n", file = stderr())
+
+  #get list of just samples in both geog and pwp matrices
+  sampstouse <- merge(rownames(geoDist) %>% as.data.frame() %>% mutate(x = "geo") %>% dplyr::rename("samp" = "."),
+                      rownames(pwp) %>% as.data.frame() %>% mutate(y = "geno") %>% dplyr::rename("samp" = "."),
+                      by = "samp", all.x = T, all.y = T) %>% 
+    filter(is.na(x)==F & is.na(y)==F) %>% 
+    arrange(samp)
+  sampstouse <- sampstouse$samp
+  #keep just samps in both geog and pwp matrices
+  geoDist <- geoDist[rownames(geoDist) %in% sampstouse, colnames(geoDist) %in% sampstouse]
+  pwp <- pwp[rownames(pwp) %in% sampstouse, colnames(pwp) %in% sampstouse]
+  #and order pwp and geogDist the same/to match
+  geoDist.ordered <- geoDist[sampstouse, sampstouse]
   geoDist <- geoDist.ordered
+  pwp.ordered <- pwp[sampstouse, sampstouse]
+  pwp <- pwp.ordered
+  cat("here4\n", file = stderr())
+  #check ordered same now
   if (identical(row.names(pwp),row.names(geoDist)) == TRUE) {
     print("names and order of rownames for pwp and geoDist match")
   } else {
     print("ERROR ! - names and/or order of rownames for pwp and geoDist do not match!")
   }
-   cat("here5\n", file = stderr())
+  cat("here5\n", file = stderr())
+  
+  #convert pwp to hom
+  hom <- 1-pwp
+  cat("here6\n", file = stderr())
+  diag(hom) <- 1 # add inbreeding
+  flag <- 0
+  scl_min <- min(hom)
+  scl_max <- max(hom-scl_min)
+  tmphom <- (hom-scl_min)/scl_max
+  #check if hom is positive definite
+  posdefcheck <- any(eigen(tmphom)$values < 0)
+  while(posdefcheck & flag < 100){
+    diag(hom) <- diag(hom) + 1e-3
+    tmphom <- (hom-scl_min)/scl_max
+    posdefcheck <- any(eigen(tmphom)$values < 0)
+    flag <- flag + 1
+  }
+  if(posdefcheck){
+    cat("ERROR: I couldn't get the dataset to be positive definite\n")
+  } else {
+    print("dataset is positive definite")
+  }
+  cat("here7\n", file = stderr())
+  #se <- popgenstats$pwp$se
+  
+  #run model
   if (model_flavor == "wishart") {
     
     # make dataBlock for stan
@@ -174,6 +193,10 @@ cat("here1\n", file = stderr())
     
   }
 
+  #clean up at end of loop iteration in case something goes awry
+  rm(popgenfile,stacksparams,sampkey,Npolyloci,
+     geoDist,geoDistnames,geoDist.ordered,
+     pwp.ordered,hom,pwp,sampstouse)
 
 }
 
