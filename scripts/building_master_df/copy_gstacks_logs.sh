07014f65c90afe2207ed48f2ef3d705288a5ead8
Rachel Toczydlowski <rhtoczydlowski@gmail.com>
2023-10-18 13:57:15 -0500

    fixed script to grab gstacks logs - same issue that we were missing those with stacks n > 9, then rebuilt methods pred df and master df

--- scripts/building_master_df/copy_gstacks_logs.sh
+++ scripts/building_master_df/copy_gstacks_logs.sh
@@ -3,6 +3,9 @@
 #define variables
 list_of_datasets=list-allfinal115.txt
 outdir=/Users/rachel/divdiv/data/all_r80_gstacksoutlogs
+storagenode=divdiv_datafiles: 
+#note - storagenode needs a / at end if it's a non Google Drive file path
+
 
 mkdir -p $outdir
 
@@ -14,8 +17,8 @@ do
 	link_name=$(echo "${run_name//bioprj_/}")
 	
 	r80dir=$storagenode/$run_name/popgen
-	r80paramstemp=$(rclone cat divdiv_datafiles:$run_name/popgen/r80params.txt)
-	r80params=$(echo "${r80paramstemp//_n[0-9]/}")
+	r80paramstemp=$(rclone cat $r80dir/r80params.txt)
+	r80params=$(echo $r80paramstemp | sed 's/_n[0-9]\{1,\}//g') #remove the _n[#]_ chunk so it matches stacks params formatting in file names elsewhere
 	
 	echo "run_name: $run_name"
 	echo "link_name: $link_name"
