0740d8c6e96bd6e8ad4e8fd47fc5574becfdada8
Rachel Toczydlowski <rhtoczydlowski@gmail.com>
2024-05-07 20:17:20 -0500

    more archive stuff

--- scripts/5_building_archives/generate_suppmat_maps_per_dataset.R
+++ scripts/5_building_archives/generate_suppmat_maps_per_dataset.R
@@ -9,13 +9,29 @@ library(tidyr)
 rm(list=ls())
 gc()
 
+setwd("/Users/rachel/divdiv/")
 
+
+
+#define variables
 
 #define path to .csv's of lat/long after running marmap (and moving landpoints to water)
-postcoordsdir = "/Users/rachel/divdiv/data/abiotic/input_and_working/postmarmap_coords"
+postcoordsdir = "data/abiotic/input_and_working/postmarmap_coords"
+
+#define path to .csv's of lat/long for GBIF 
+#(cleaned but no points moved or dropped for marmap aka the files that went into calcing n.ecoregions and mean/min/max lat)
+gbifdir = "data/abiotic/input_and_working/speciesLocDat_Sep08_2021_withFilters-cleaned/"
 
 #define path to save figs to
-outdir = "/Users/rachel/divdiv/suppmat_figures/maps"
+outdir = "suppmat_figures/maps"
+
+#define path to WM files (so we can get genetic samples we kept in end)
+wmpath = "data/popgen/input_and_working/ALL_r80_gendiv_data"
+
+#define path to sample name keys
+sampkeypath = "data/all_samplenamekeys"
+
+
 
 #get world map outlines
 coasts <- rnaturalearth::ne_coastline(scale = "medium", returnclass = "sf")
@@ -32,13 +48,15 @@ ggplot() +
   geom_sf(data = gls)
 
 #get df of range mean lats
-meanlat <- read.csv("/Users/rachel/divdiv/data/abiotic/mean_min_max_latitude_stats-wide.csv") %>% dplyr::select(-X)
+meanlat <- read.csv("data/abiotic/mean_min_max_latitude_stats-wide.csv") %>% dplyr::select(-X)
 
 #get final list of "in" datasets from master df
-using <- read.csv("/Users/rachel/divdiv/data/master_df.csv")
-dataset_list <- using$link
+using <- read.csv("data/master_df.csv")
+dataset_list <- using$run_name
 
 
+
+#build PDF map for each dataset
 for ( loop.iter in 1:length(dataset_list) ) {
   
   #get datasets loaded
@@ -49,18 +67,43 @@ for ( loop.iter in 1:length(dataset_list) ) {
   print(paste0("starting to plot: ", run_name, "; ", loop.iter, " of ", length(dataset_list)))
   species = run_name %>% strsplit(., "_") %>% as.data.frame() %>% .[nrow(.),] %>% gsub("-"," ",.)
   
-  gbif.post <- read.csv(paste(postcoordsdir,"/gbif-coordspostmarmap-",run_name,".csv",sep=""))
+  #get GBIF and genetic points to map
+  gbif.clean <- read.csv(paste(gbifdir,"/GBIFLocations_",species,".csv",sep=""))
   genetic.post <- read.csv(paste(postcoordsdir,"/genetic-coordspostmarmap-",run_name,".csv",sep=""))
+  
+  #get list of samples retained for popgen/gendiv calcs (so we only plot just genetic samples we used/kept in end)
+  wmfile <- list.files(path = wmpath, pattern = run_name, full.names = TRUE)
+  wmfile <- wmfile[which(grepl("initPars", wmfile)==T)]
+  load(wmfile)
+  if (dim(initPars$parHom)[1] != dim(initPars$parHom)[2]) {"error - matrix dims are not the same"}
+  n_samples = dim(initPars$parHom)[1] #print this in title of final map PDF later in this script
+  geneticsampskept <- rownames(initPars$parHom) %>% as.data.frame() %>% dplyr::rename("sampid_assigned_for_bioinf" = ".")
+  #get sample name match key (to match SRR and sample0 name formats)
+  sampkeyFile <- paste0(sampkeypath,"/samplenamekey-",run_name,".txt")
+  sampkeyFile <- read.delim(sampkeyFile)
+  geneticsampskept <- merge(geneticsampskept, 
+                            sampkeyFile %>% dplyr::select(sampid_assigned_for_bioinf, run_acc_sra),
+                            by = "sampid_assigned_for_bioinf")
+  #filter to genetic lat/long df to keep just these samples (aka ones we kept in popgen)
+  genetic.post <- genetic.post %>% filter(run_acc_sra %in% geneticsampskept$run_acc_sra)
+  
+  #merge into one dataset to make aes easier
+  gbif.clean.plot <- gbif.clean %>% 
+    dplyr::rename("run_acc_sra"="sampid", "x"="decimalLongitude", "y"="decimalLatitude") %>% 
+    dplyr::select(run_acc_sra, x, y) %>% mutate(dataset = "gbif")
+  genetic.post.plot <- genetic.post %>% 
+    dplyr::select(run_acc_sra, x, y) %>% mutate(dataset = "genetic")
+  points <- rbind(gbif.clean.plot, genetic.post.plot)
   
   #get range mean lat of GBIF points
   meanlat.sp <- meanlat %>% filter(link == link.i)
   meanlat.sp <- meanlat.sp$meanlat.gbif
   
   #set bounds for inset map aka zoomed in map of sample locations
-  maxlat <- max(gbif.post$y, genetic.post$y) + 10
-  minlat <- min(gbif.post$y, genetic.post$y) - 30
-  maxlon <- max(gbif.post$x, genetic.post$x) + 10
-  minlon <- min(gbif.post$x, genetic.post$x) - 30
+  maxlat <- max(points$y) + 10
+  minlat <- min(points$y) - 30
+  maxlon <- max(points$x) + 10
+  minlon <- min(points$x) - 30
   if (maxlat > 90) {maxlat = 90}
   if (minlat < -90) {minlat = -90}
   if (maxlon > 180) {maxlon = 180}
@@ -70,8 +113,8 @@ for ( loop.iter in 1:length(dataset_list) ) {
   inset.plot <- ggplot() + 
     #build map
     geom_sf(data = land, fill = "gray90", colour = "gray90", linewidth = 0.2) +
-    geom_sf(data = coasts, fill = "transparent", colour = "gray70", linewidth = 0.2) +
-    geom_sf(data = gls, fill = "white", colour = "gray70", linewidth = 0.3) +
+    geom_sf(data = coasts, fill = "transparent", colour = "gray80", linewidth = 0.2) +
+    geom_sf(data = gls, fill = "white", colour = "gray80", linewidth = 0.3) +
     scale_y_continuous(expand = c(0, 0)) +
     scale_x_continuous(expand = c(0, 0)) +
     #add zoom box
@@ -91,27 +134,28 @@ for ( loop.iter in 1:length(dataset_list) ) {
   main.plot <- ggplot() + 
     #build map
     geom_sf(data = land, fill = "gray90", colour = "gray90", linewidth = 0.2) +
-    geom_sf(data = coasts, fill = "transparent", colour = "gray70", linewidth = 0.2) +
-    geom_sf(data = gls, fill = "white", colour = "gray70", linewidth = 0.3) +
+    geom_sf(data = coasts, fill = "transparent", colour = "gray80", linewidth = 0.2) +
+    geom_sf(data = gls, fill = "white", colour = "gray80", linewidth = 0.3) +
     #add mean lat of GBIF points
     geom_hline(yintercept = meanlat.sp, colour = "black", linewidth = 0.4, linetype = 2) +
-    #add points
-    geom_point(data = gbif.post, aes(x=x, y=y, color = "blue"), alpha = 0.35, size = 1) +
-    geom_point(data = genetic.post, aes(x=x, y=y, color = "magenta"), alpha = 1, size = 1) +
+    #add points (last two values of hex aka 60 specify alpha/opacity)
+    geom_point(data = points, aes(x=x, y=y, colour=dataset), size = 1) +
+    scale_colour_manual(values = c("#0000FF60", "magenta"), 
+                        labels = c("Occurrence reports (GBIF)", "Genetic samples (INSDC)")) +
     #manually make a legend for points and lines
-    scale_color_identity(guide = "legend",
-                         name = "",
-                         breaks = c("blue", "magenta"),
-                         labels = c("Occurrence reports (GBIF)", "Genetic samples (INSDC)")) +
     labs(title = paste0("Dataset: ", run_name),
-         subtitle = paste0("N = ",nrow(genetic.post)," genetic samples"),
+         subtitle = paste0("N = ",n_samples," genetic samples; dashed line shows mean range latitude (occurrence records)"),
          x = "Longitude",
          y = "Latitude") +
     theme_bw() +
     theme(panel.grid = element_blank(),
           legend.position = c(0.88, 0.08),
-          legend.background = element_blank()) +
-    guides(fill = "none") +
+          legend.background = element_blank(),
+          legend.box.background = element_rect(fill = "white"),
+          legend.margin = margin(0, 10, 5, 10),
+          legend.key = element_blank(),
+          legend.title = element_blank()
+          ) +
     coord_sf(xlim = c(minlon,maxlon),
              ylim = c(minlat,maxlat))
   
@@ -124,9 +168,12 @@ for ( loop.iter in 1:length(dataset_list) ) {
   print(plot)
   dev.off()
   rm(maxlat,maxlon,minlat,minlon,run_name,species,
-     bathymapZOOMED,bathymapZOOMED.simple,
-     gbif.post,genetic.post,
-     main.plot,inset.plot)
+     gbif.clean,genetic.post,
+     geneticsampskept,initPars,plot,
+     sampkeyFile,meanlat.sp,n_samples,wmfile,
+     gbif.clean.plot,genetic.post.plot,points,
+     main.plot,inset.plot,
+     link.i,dataset)
   
 }
 
